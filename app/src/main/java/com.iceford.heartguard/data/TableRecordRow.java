dda065bd052d38ad6317ae21c9fa7308caf4167e
WhimsyQuester <rongquanhuang01@gmail.com>
2024-08-15 20:25:32 +0800

    TableRecordRow

---
+++ app/src/main/java/com.iceford.heartguard/data/TableRecordRow.java
@@ -0,0 +1,61 @@
+package com.iceford.heartguard.data;
+
+import static com.iceford.heartguard.utils.FormatUtil.roundToInteger;
+import static com.iceford.heartguard.utils.TimeUtil.timestampToBeijingTime;
+
+public class TableRecordRow {
+    private final String measureTime;
+    private final String duration;
+    private final int minHR;
+    private final double avgHR;
+    private final int maxHR;
+
+    private final double avgHRV;
+    private final double avgQTC;
+    private final double avgSTRESS;
+
+    public TableRecordRow(String measureTime, String duration, int minHR, double avgHR, int maxHR, double avgHRV, double avgQTC, double avgSTRESS) {
+        this.measureTime = measureTime;
+        this.duration = duration;
+        this.minHR = minHR;
+        this.avgHR = avgHR;
+        this.maxHR = maxHR;
+        this.avgHRV = avgHRV;
+        this.avgQTC = avgQTC;
+        this.avgSTRESS = avgSTRESS;
+    }
+
+
+    public String getMeasureTime() {
+        return timestampToBeijingTime(Long.parseLong(measureTime));
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public int getMinHR() {
+        return minHR;
+    }
+
+    public int getAvgHR() {
+        return roundToInteger(avgHR);
+    }
+
+    public int getMaxHR() {
+        return maxHR;
+    }
+
+    public int getAvgHRV() {
+        return roundToInteger(avgHRV);
+    }
+
+    public int getAvgQTC() {
+        return roundToInteger(avgQTC);
+    }
+
+    public int getAvgStress() {
+        return roundToInteger(avgSTRESS);
+    }
+}
+
